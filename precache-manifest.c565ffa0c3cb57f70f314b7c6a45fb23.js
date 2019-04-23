6b49227d745fb799747e06e9e694484582c565a0
Emroze <emrozesd@users.noreply.github.com>
2019-04-23 15:31:22 -0700

    Updates

---
+++ precache-manifest.c565ffa0c3cb57f70f314b7c6a45fb23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5b9dc0d4d740e96f6452495766311877",
+    "url": "/Bobs-Burgers-Memory-Game/static/media/bb-banner-1.5b9dc0d4.jpg"
+  },
+  {
+    "revision": "977fd31488b7ecb19ace",
+    "url": "/Bobs-Burgers-Memory-Game/static/js/runtime~main.5f8df999.js"
+  },
+  {
+    "revision": "df60d06d1db333fee112",
+    "url": "/Bobs-Burgers-Memory-Game/static/js/main.6b8a2f96.chunk.js"
+  },
+  {
+    "revision": "bdc426c3d81b3cc97d92",
+    "url": "/Bobs-Burgers-Memory-Game/static/js/2.8fa3d35f.chunk.js"
+  },
+  {
+    "revision": "df60d06d1db333fee112",
+    "url": "/Bobs-Burgers-Memory-Game/static/css/main.b2f1c228.chunk.css"
+  },
+  {
+    "revision": "71d0b9e7da0bbbd393d6aa1799b01dd9",
+    "url": "/Bobs-Burgers-Memory-Game/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Bobs-Burgers-Memory-Game/precache-manifest.c565ffa0c3cb57f70f314b7c6a45fb23.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Bobs-Burgers-Memory-Game/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
